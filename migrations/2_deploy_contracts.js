e06ccda3b3c34a98780594bf73cb0c48785ef981
Professor-FernandoLopes <datacurrency@datacurrency.uk>
2022-02-10 05:31:40 -0300

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,9 @@
-const SafemoonFork = artifacts.require('CampaignFactory.sol');
+const CampaignFactory = artifacts.require('CampaignFactory.sol');
 
 module.exports = function (deployer, network) {
   if(network === 'bscTestnet') {
-    deployer.deploy(SafemoonFork, '0x3b5805a6Af5E3E262FbB45362FDf3C501919B658','0x1a3293B02D9113A4023fDA12C1380c99ce597143');
+    deployer.deploy(CampaignFactory, '0x3b5805a6Af5E3E262FbB45362FDf3C501919B658','0x1a3293B02D9113A4023fDA12C1380c99ce597143');
   } else {
-    deployer.deploy(SafemoonFork, '0x3b5805a6Af5E3E262FbB45362FDf3C501919B658','0x1a3293B02D9113A4023fDA12C1380c99ce597143');
+    deployer.deploy(CampaignFactory, '0x3b5805a6Af5E3E262FbB45362FDf3C501919B658','0x1a3293B02D9113A4023fDA12C1380c99ce597143');
   }
 };
